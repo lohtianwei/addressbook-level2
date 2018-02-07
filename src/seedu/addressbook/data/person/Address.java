a87d31888a5b9b98f48b3efc9874ac3d53463d6d
Tianwei <lohtianwei@msn.com>
2018-02-08 00:05:42 +0800

    change address class to contain additional fields. Untested.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -12,7 +12,12 @@ public class Address {
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
+    public final int block;
+    public final String street;
+    public final String unit;
+    public final String postalCode;
+
+
     private boolean isPrivate;
 
     /**
@@ -20,14 +25,16 @@ public class Address {
      *
      * @throws IllegalValueException if given address string is invalid.
      */
-    public Address(String address, boolean isPrivate) throws IllegalValueException {
-        String trimmedAddress = address.trim();
+    public Address(int block, String street, String unit, String postalCode, boolean isPrivate) throws IllegalValueException {
+
         this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
-            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        }
-        this.value = trimmedAddress;
+        this.block = block;
+        this.street = street;
+        this.unit = unit;
+        this.postalCode = postalCode;
     }
+
+
 
     /**
      * Returns true if a given string is a valid person address.
@@ -38,9 +45,9 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block+street+unit+postalCode;
     }
-
+/*
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -51,9 +58,25 @@ public class Address {
     @Override
     public int hashCode() {
         return value.hashCode();
-    }
+    }*/
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    public int getBlock(){
+        return block;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 }
